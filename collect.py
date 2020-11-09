dd9e8f54cb3723f258b6995e41b798e535d79e10
Tisloy <pmylanimation@gmail.com>
2020-11-09 18:59:22 +0900

    Fixed Bug, Add proper sEMG dataset sampled from a board, Changed file dir

--- collect.py
+++ collect.py
@@ -1,76 +1,79 @@
 import serial
 import os
 import time
+import matplotlib.pyplot as plt
 
+
+# Open Serial Port
 try:
     ser = serial.Serial("COM6", 115200, timeout=1)
     print(f"serial open? {ser.isOpen()}")
 except:
     print("serial error")
 
-result_list = []  # [time, gesture, ch1, ch2, ch1, ch2, ..., ch1, ch2]
-setTime = 2  # [s]
-preparationTime = 2  # [s]
-ch_n = 2
+result = []  # [time, gesture, ch1, ch2, ch1, ch2, ..., ch1, ch2]
+collectingTime = 2  # Time for Sampling [s]
+preparationTime = 2  # Time for next gesture preparation[s]
+n_ch = 2  # number of channel
 gesture_name = ['Noise', 'Fist', 'Hand', 'Index Finger', 'Middle Finger', 'Little Finger']
-data_iter = 5
+data_iter = 5  # number of sampling loop   
 
+# find current time
 now = index_time = time.time()
 
-## Collecting Data
+# Collect sEMG data
 for num in range(data_iter):
     print(f"===========Step {num+1}/{data_iter}===========")
 
     for iii in range(1, len(gesture_name)):
-        ### Pause
+        # Collect noisy sEMG
         start_time = now
         print(f"pause for now...")
         temp = []
-        while now - start_time < setTime:
-            temp.extend([float(i) for i in ser.readline().decode().strip().split(',')[0:ch_n]])
+        while now - start_time < collectingTime:
+            temp.extend([float(i) for i in ser.readline().decode().strip().split(',')[0:n_ch]])
             now = time.time()
-        result_list.append([now - index_time, 0] + temp)
 
+        # Add noisy sEMG to result
+        result.append([now - index_time, 0] + temp)
 
-        ### Collect Gesture Data
+        # Collect gesture sEMG
         start_time = now
         print(f"Get ready for gesture: {gesture_name[iii]}")
         temp = []
         while now - start_time < preparationTime:
-            temp.extend([float(i) for i in ser.readline().decode().strip().split(',')[0:ch_n]])
+            temp.extend([float(i) for i in ser.readline().decode().strip().split(',')[0:n_ch]])
             now = time.time()
         start_time = now
         print(f"Collecting gesture: {gesture_name[iii]}")
-        while now - start_time < setTime:
-            temp.extend([float(i) for i in ser.readline().decode().strip().split(',')[0:ch_n]])
+        while now - start_time < collectingTime:
+            temp.extend([float(i) for i in ser.readline().decode().strip().split(',')[0:n_ch]])
             # print(f"Collecting gesture {gesture_name[iii]} : {temp}")
             now = time.time()
         start_time = now
         while now - start_time < preparationTime:
-            temp.extend([float(i) for i in ser.readline().decode().strip().split(',')[0:ch_n]])
+            temp.extend([float(i) for i in ser.readline().decode().strip().split(',')[0:n_ch]])
             now = time.time()
-        result_list.append([now - index_time, iii] + temp)
 
+        # Add gesture sEMG to result
+        result.append([now - index_time, iii] + temp)
 
-### Saving Data
+# Save Data
 path = os.getcwd() + "\\data\\tmp\\"
 file_name = time.strftime("%Y%m%d_%H%M%S") + ".txt"
 print(path+file_name)
 with open(path + file_name, 'w') as f:
-    for data in result_list:
+    for data in result:
         s = ", ".join(map(str, data))
         f.write("%s\n" % s)
 f.close()
 
-
-### Graph Plotting
-import matplotlib.pyplot as plt
-
+# Plot Graph
 plot_index = [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2]]
 label_num = [0] * len(gesture_name)
 fig, axs = plt.subplots(2, 3, figsize=(30, 20))
 fig.suptitle('Sampled EMG Ch1', fontsize=20)
-for data in result_list:
+for data in result:
     g = int(data[1])
     axs[plot_index[g][0], plot_index[g][1]].plot(range(len(data[2::2])), data[2::2], label=f"{g}_#{label_num[g]}")
     label_num[g] += 1
@@ -86,7 +89,7 @@ for ax in axs.flat:
 label_num = [0] * len(gesture_name)
 fig, axs = plt.subplots(2, 3, figsize=(30, 20))
 fig.suptitle('Sampled EMG Ch2', fontsize=20)
-for data in result_list:
+for data in result:
     g = int(data[1])
     axs[plot_index[g][0], plot_index[g][1]].plot(range(len(data[3::2])), data[3::2], label=f"{g}_#{label_num[g]}")
     label_num[g] += 1
--- model_test.py
+++ model_test.py
@@ -4,72 +4,59 @@ import matplotlib.pyplot as plt
 import serial
 import os
 import time
-import keyboard
 import random
 
-## Open Serial Port
+# Open Serial Port
 try:
     ser = serial.Serial("COM6", 115200, timeout=1)
     print(f"serial open? {ser.isOpen()}")
 except:
     print("serial error")
 
-## Initial Setting
-#gesture_name = ['Noise', 'Fist', 'Index Finger', 'Middle Finger', 'Ring Finger', 'Little Finger', 'Hand']
+
 gesture_name = ['Noise', 'Fist', 'Hand', 'Index Finger', 'Middle Finger', 'Little Finger']
-gesture_iter = 20       # Number of gesture sampling
-ch_n = 2
-update_len = 1          # Number of EMG Samples before updating CNN input, set for latency 0.1s
-true_glab_list = [0]    # True Gesture Label
-pred_glab_list = [0]    # Model Prediction Gesture Label
-acc = [0.0]             # Realtime Model Accuracy
+n_gesture_test = 3  # Number of gestures used in a RT model accuracy
+n_ch = 2  # Number of channels
+update_len = 1  # Number of sEMG data required for the next CNN input, default : latency 0.1s
+flushTime = 1  # Time for flushing old sEMG data
+testTime = 3  # Time for classifying gestures
+true_label = []  # True label
+pred_label = []  # Predicted label
+acc = []  # Model RT Accuracy
 
 
 # Generate Random gesture label
-g_label = [0]
-for i in range(gesture_iter-1):
+g_label = [random.randrange(0, len(gesture_name), 1)]
+for i in range(n_gesture_test-1):
     tmp = random.randrange(0, len(gesture_name), 1)
-    while tmp == g_label[i] :
+    while tmp == g_label[i]:
         tmp = random.randrange(0, len(gesture_name), 1)
     g_label.append(tmp)
 
 
-## Load Trained Model
-path = os.getcwd() + "\\data\\Model\\"
+# Load Trained model
+path = os.getcwd() + "\\data\\model\\"
 model = tf.keras.models.load_model(path+'my_model')
 model.summary()
-seq_len = model.layers[0].input.shape[1]  # CNN input size(window size)
+seq_len = model.layers[0].input.shape[1]  # CNN input size(window length)
 
 
-## Receive EMG from Cyton Board
-emgSignal = np.empty([1, seq_len, ch_n])
+# Receive sEMG from a Cyton Board
+emgSignal = np.empty([1, seq_len, n_ch])
 start = time.time()
 for i in range(seq_len):
-    emgSignal[0, i, :] = [float(i) for i in ser.readline().decode().strip().split(',')[0:ch_n]]
+    emgSignal[0, i, :] = [float(i) for i in ser.readline().decode().strip().split(',')[0:n_ch]]
 now = time.time()
-print(f"Full Data Received : {now - start}s")
+print(f"Full Data Received at {now - start}s")
 
 
-fig, (ax1, ax2) = plt.subplots(1, 2)
-graph_x = [0]
-for ii in range(gesture_iter):
-    '''
-    if keyboard.is_pressed('q'):
-        print(f"Exit Program")
-        break
-    elif keyboard.is_pressed('a'):
-        print(f"Press s to continue...")
-        while True:
-            if keyboard.is_pressed('s'):
-                break
-    '''
-
-    # Pause to flush emgSignal
+for ii in range(n_gesture_test):
+    # Flush old sEMG
     print("pause for now...")
     tol = 0
-    while tol <= 1:
+    while tol <= flushTime:
         start = time.time()
-        emgSignal = np.append(emgSignal, [[[float(i) for i in ser.readline().decode().strip().split(',')[0:ch_n]]]],
+        emgSignal = np.append(emgSignal, [[[float(i) for i in ser.readline().decode().strip().split(',')[0:n_ch]]]],
                               axis=1)
         emgSignal = np.delete(emgSignal, 0, axis=1)
         tol += time.time() - start
@@ -77,67 +64,76 @@ for ii in range(gesture_iter):
     print(f"Get ready for gesture: {gesture_name[g_label[ii]]}")
     time.sleep(2)
 
-    # Receiving Signal and Classifying
+    # Classify gestures
     print(f"Collecting gesture: {gesture_name[g_label[ii]]}")
     tol = 0
-    while tol <= 3:
+    while tol <= testTime:
         start = time.time()
         for i in range(update_len):
-            emgSignal = np.append(emgSignal, [[[float(i) for i in ser.readline().decode().strip().split(',')[0:ch_n]]]],
+            emgSignal = np.append(emgSignal, [[[float(i) for i in ser.readline().decode().strip().split(',')[0:n_ch]]]],
                                   axis=1)
         emgSignal = np.delete(emgSignal, np.s_[0:update_len], axis=1)
 
         # Gesture Classification
         predictions = model.predict(emgSignal)[0]
-        pred_g_label = int(np.where(predictions == max(predictions))[0])
+        prediction_label = int(np.where(predictions == max(predictions))[0])
 
         # Save Results
-        pred_glab_list.append(pred_g_label)
-        true_glab_list.append(g_label[ii])
-        if pred_g_label == g_label[ii]:
-            acc.append((len(acc)*acc[-1] + 1)/(len(acc)+1))
+        pred_label.append(prediction_label)
+        true_label.append(g_label[ii])
+
+        # Update Model RT accuracy
+        if len(acc) != 0:
+            if prediction_label == g_label[ii]:
+                acc.append((len(acc) * acc[-1] + 1) / (len(acc) + 1))
+            else:
+                acc.append((len(acc) * acc[-1]) / (len(acc) + 1))
         else:
-            acc.append((len(acc)*acc[-1])/(len(acc)+1))
+            acc.append(float(prediction_label == g_label[ii]))
 
-        graph_x.append(ii)
-        '''
-        # Realtime Graph Plotting
-        ax1.plot(graph_x, acc)
-        ax1.lines[0].set_xdata(graph_x)
-        ax1.lines[0].set_ydata(acc)
-
-        line1, = ax2.plot(graph_x, pred_glab_list)
-        line2, = ax2.plot(graph_x, true_glab_list)
-        ax2.legend((line1, line2), ('prediction', 'true'))
-        ax2.lines[0].set_xdata(graph_x)
-        ax2.lines[0].set_ydata(pred_glab_list)
-
-        fig.gca().relim()
-        fig.gca().autoscale_view()
-        plt.pause(0.0001)
-        '''
         tol += time.time() - start
         # Show Result
-        print("Eclipsed Time : {:.3f} \t Predicted: {:d} \t True: {:d} \t Accuracy : {:.3f}".format(tol, pred_g_label, g_label[ii], acc[-1]))
+        print("Eclipsed Time : {:.3f} \t Predicted: {:d} \t True: {:d} \t Accuracy : {:.3f}".format(tol, prediction_label, g_label[ii], acc[-1]))
 
 
-print("Realtime Accuracy : {:.3f}".format(acc[-1]))
-
-# Plot Graph
-fig, (ax1, ax2) = plt.subplots(1, 2)
-ax1.plot(graph_x, acc)
-line1, = ax2.plot(graph_x, pred_glab_list)
-line2, = ax2.plot(graph_x, true_glab_list[:len(true_glab_list)])
-ax2.legend((line1, line2), ('prediction', 'true'), loc = 'upper right')
-plt.show()
-
-# Plot Graph
+# Plot Model RT accuracy
 fig = plt.figure()
-line1, = plt.plot(graph_x, pred_glab_list)
-line2, = plt.plot(graph_x, true_glab_list[:len(true_glab_list)])
-fig.legend((line1, line2), ('prediction', 'true'), loc = 'upper right')
+line1, = plt.plot(acc, label = 'RT Accuracy')
+plt.legend(loc = 'upper right')
+plt.title('Model RT Accuracy')
+plt.xlabel('Data')
+plt.ylabel('Label')
 plt.show()
 
-tmp = np.delete(np.array(pred_glab_list), np.where(np.array(pred_glab_list) == 0))
-tmp2 = np.delete(np.array(true_glab_list), np.where(np.array(pred_glab_list) == 0))
-print("Accuracy Removed Zero : {:.3f}".format(sum(tmp == tmp2)/len(tmp)))
+
+# Plot Classification Result Graph
+fig = plt.figure()
+plt.plot(pred_label, label = 'Predicted Label')
+plt.plot(true_label, label = 'True Label')
+plt.legend(loc = 'upper right')
+plt.title('Classification Result Graph')
+plt.xlabel('Data')
+plt.ylabel('Label')
+plt.show()
+
+print("Model RT Accuracy on {:d} data : {:.3f}%".format(len(pred_label), 100*acc[-1]))
+tmp = np.delete(np.array(pred_label), np.where(np.array(pred_label) == 0))
+tmp2 = np.delete(np.array(true_label), np.where(np.array(pred_label) == 0))
+print("Model RT Accuracy on {:d} data (Removed Zero Label): {:.3f}%".format(len(pred_label), 100*sum(tmp == tmp2)/len(tmp)))
+
+'''
+# Realtime Graph Plotting
+ax1.plot(graph_x, acc)
+ax1.lines[0].set_xdata(graph_x)
+ax1.lines[0].set_ydata(acc)
+
+line1, = ax2.plot(graph_x, pred_label)
+line2, = ax2.plot(graph_x, true_label)
+ax2.legend((line1, line2), ('prediction', 'true'))
+ax2.lines[0].set_xdata(graph_x)
+ax2.lines[0].set_ydata(pred_label)
+
+fig.gca().relim()
+fig.gca().autoscale_view()
+plt.pause(0.0001)
+'''
